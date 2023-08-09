2d9483691197d3ea7d1464935054a337321d6d93
Maxwella10 <96734464+Maxwella10@users.noreply.github.com>
2023-08-09 16:31:30 +0100

    setup variables in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var cityInput = document.querySelector(".cityInput");
+var submitBtn = document.querySelector(".submitBtn");
+var historyUl = document.querySelector(".historyUl");
+var cityInfo = document.querySelector(".cityInfo");
+var icon = document.querySelector(".icon");
+var temp = document.querySelector(".temp");
+var wind = document.querySelector(".wind");
+var humidity = document.querySelector(".humidity");
+var forecastCard = document.querySelector(".forecastCard");
+
 var key = "d025108d5b5bff4e0d318b8b71ba2019";
-var currentData = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
-var forecastData = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}`
+var currentData = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+var forecastData = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}`;
