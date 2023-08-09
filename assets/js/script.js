c86084d5b2e034154a1dd3faff298a170ac01eb1
Maxwella10 <96734464+Maxwella10@users.noreply.github.com>
2023-08-09 16:26:45 +0100

    set up html for webpage

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+var key = "d025108d5b5bff4e0d318b8b71ba2019";
+var currentData = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+var forecastData = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}`
