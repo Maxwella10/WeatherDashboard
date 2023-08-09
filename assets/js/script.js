61e9924f0f3b3c4b1cecae0606c316534fef3b16
Maxwella10 <96734464+Maxwella10@users.noreply.github.com>
2023-08-09 16:37:14 +0100

    fetching data for current and future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,29 @@ var humidity = document.querySelector(".humidity");
 var forecastCard = document.querySelector(".forecastCard");
 
 var key = "d025108d5b5bff4e0d318b8b71ba2019";
-var currentData = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
-var forecastData = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}`;
+
+submitBtn.addEventListener("click", function() {
+    var cityName = cityInput.value;
+    getCurrentData(cityName);
+    getForecastData(cityName);
+});
+
+function getCurrentData(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+}
+
+function getForecastData(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+}
